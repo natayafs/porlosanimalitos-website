f23ad78dc717da5ffebc78af9bd77e99f6556f9f
Nataya Soledad <natayafs@gmail.com>
2021-03-09 19:15:28 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,11 @@ import { SlidesroutesComponent } from './slidesroutes/slidesroutes.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { AboutComponent } from './about/about.component'
-import { ProyectoComponent } from './Proyecto/proyecto.component'
 
 const routes: Routes = [
   { path: '', component: SlidesroutesComponent },
   { path: 'home', component: WelcomeComponent },
   { path: 'nosotros', component: AboutComponent },
-  { path: 'proyecto', component: ProyectoComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: '**', component: PagenotfoundComponent },
 ];
